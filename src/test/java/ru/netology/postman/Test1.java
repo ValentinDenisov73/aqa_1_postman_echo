b04a9a9241a2f7bb6392ea034d66b8cd2d110b2e
Valentin Denisov <valentindenisov1989@gmail.com>
2023-01-11 22:01:56 +0400

    fix test to pass

--- src/test/java/ru/netology/postman/Test1.java
+++ src/test/java/ru/netology/postman/Test1.java
@@ -20,7 +20,7 @@ public class Test1 {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello Word"))
+                .body("data", equalTo("Hello World"))
         ;
     }
 }
